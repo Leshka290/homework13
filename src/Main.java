a791de3ea9757f98f0a0bf5e9af30a29ddb77d23
leshka290 <lleesshhkkaa00@gmail.com>
2023-04-03 08:52:10 +0300

    Доработки equals and hashCode

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+
 public class Main {
     public static void main(String[] args) {
 
+        Author joanneRowling = new Author("Joanne", "Rowling");
+        Author johnRonald = new Author("John", "Ronald Reuel Tolkien");
+        Book harryPotter = new Book("Harry Potter and the Philosopher's Stone",
+                joanneRowling, 1997);
+        Book theLordOfTheRings = new Book("The Lord of the Rings",
+                johnRonald, 1954);
+        theLordOfTheRings.setDateCreate(1955);
+        System.out.println(harryPotter);
+        System.out.println(joanneRowling);
     }
 }
